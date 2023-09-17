2e45851e3529115dc83060f5e6d3a3e99433aa1f
bbgard <bgardiner@students.federation.edu.au>
2023-09-17 10:32:31 +1000

    Initial Commit

---
+++ ITECH-3109-Assignment2/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * This is the main class for running the ITECH-3109 Assignment 2 code.
+ * 
+ * @author Benjamin Gardiner - Student ID: 30399545
+ */
+public class Main {
+
+	
+	/**
+	 * Main method
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
